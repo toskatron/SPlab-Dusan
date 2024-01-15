1dba007c947f1b7e348b1e5e71d0dc99ad869b25
toskatron <dusan.teicu02@e-uvt.ro>
2024-01-15 14:54:50 +0200

    Lab6

--- src/main/java/com/example/splab/SPlabApplication.java
+++ src/main/java/com/example/splab/SPlabApplication.java
@@ -1,41 +1,57 @@
 package com.example.splab;
 
+import com.example.splab.difexample.ClientComponent;
+import com.example.splab.difexample.SingletonComponent;
+import com.example.splab.difexample.TransientComponent;
 import module.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import proxy.ImageProxy;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
 public class SPlabApplication {
 
     public static void main(String[] args) {
-
-        //SpringApplication.run(SPlabApplication.class, args);
-        long startTime = System.currentTimeMillis();
-        ImageProxy img1 = new ImageProxy("Pamela Anderson");
-        ImageProxy img2 = new ImageProxy("Kim Kardashian");
-        ImageProxy img3 = new ImageProxy("Kirby Griffin");
-        Section playboyS1 = new Section("Front Cover");
-        playboyS1.add(img1);
-        Section playboyS2 = new Section("Summer Girls");
-        playboyS2.add(img2);
-        playboyS2.add(img3);
-        Book playboy = new Book("Playboy");
-        playboy.addContent(playboyS1);
-        playboy.addContent(playboyS2);
-        long endTime = System.currentTimeMillis();
-        System.out.println("Creation of the content took " + (endTime - startTime) + " milliseconds");
-
-        startTime = System.currentTimeMillis();
-        playboyS1.print();
-        endTime = System.currentTimeMillis();
-        System.out.println("Printing of the section 1 took " + (endTime - startTime) + " milliseconds");
-
-        startTime = System.currentTimeMillis();
-        playboyS1.print();
-        endTime = System.currentTimeMillis();
-        System.out.println("Printing again the section 1 took " + (endTime - startTime) + " milliseconds");
-
+    //
+    // Run this main function and inspect the output console
+    // to learn about
+    // the lifecycle of objects within
+    // Spring Dependency Injection Context
+    //
+    // Gets a handle of dependency injection context
+            ApplicationContext context =
+                    SpringApplication.run(SPlabApplication.class, args);
+    // Gets an instance of TransientComponent from the DI context
+            TransientComponent transientBean =
+                    context.getBean(TransientComponent.class);
+            transientBean.operation();
+    // Note that every time an instance is required,
+    // the DI context creates a new one
+            transientBean = context.getBean(TransientComponent.class);
+            transientBean.operation();
+    // Gets an instance of SingletonComponent from the DI context
+    // Note that the unique instance was created while
+    // application was loaded, before creating
+    // the transient instances
+            SingletonComponent singletonBean =
+                    context.getBean(SingletonComponent.class);
+            singletonBean.operation();
+    // Note that every time an instance is required,
+    // the DI returns the same unique one
+            singletonBean = context.getBean(SingletonComponent.class);
+            singletonBean.operation();
+    // Gets an instance of another class that
+    // requires singleton/transient components
+    // Note where this instance was created and what beans
+    // were used to initialize it
+            ClientComponent c = context.getBean(ClientComponent.class);
+            c.operation();
+    // One can also request an instance from DI context by name
+            c = (ClientComponent)context.getBean("clientComponent");
+            c.operation();
 
     }
 
